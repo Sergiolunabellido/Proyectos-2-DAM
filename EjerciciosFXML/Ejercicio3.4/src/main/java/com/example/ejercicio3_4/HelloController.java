a72196c5ae7ecc893cf42ccf73cd231b48f50e35
sergio <sergiolunabellido03@gmail.com>
2025-11-12 16:19:42 +0100

    ejercicio 35 nuevo

--- EjerciciosFXML/Ejercicio3.4/src/main/java/com/example/ejercicio3_4/HelloController.java
+++ EjerciciosFXML/Ejercicio3.4/src/main/java/com/example/ejercicio3_4/HelloController.java
@@ -5,54 +5,54 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 
 public class HelloController {
     public ComboBox Colores;
     public Button Limpiar;
     public Canvas Dibujo;
-
     GraphicsContext gc;
 
     @FXML
     public void initialize() {
 
+
+        Colores.getItems().addAll("Azul", "Rojo", "Verde");
+        Colores.setValue("Colores");
         gc = Dibujo.getGraphicsContext2D();
-
-        gc.setStroke(Color.BLACK);
-
-        Colores.setOnAction(e -> {
-
-            if (Colores.getSelectionModel().toString().equals("Azul")) gc.setStroke(Color.BLUE);
-            else if (Colores.getValue().equals("Rojo")) gc.setStroke(Color.RED);
-            else if (Colores.getValue().equals("Verde")) gc.setStroke(Color.GREEN);
-            else gc.setStroke(Color.BLACK);
-
-        });
 
         gc.setLineWidth(3);
 
         Dibujo.setOnMousePressed(e -> {
-
             gc.beginPath();
             gc.moveTo(e.getX(), e.getY());
             gc.stroke();
-
         });
 
         Dibujo.setOnMouseDragged(e -> {
           gc.lineTo(e.getX(), e.getY());
           gc.stroke();
         });
+        Dibujo.setStyle("-fx-border-color: Black; -fx-border-width: 2; -fx-padding: 10;");
+
+    }
+
+    public void CambiarColor(){
+
+        if(Colores.getValue()== "Azul"){
+            gc.setStroke(Color.BLUE);
+        }else if(Colores.getValue()=="Rojo"){
+            gc.setStroke(Color.RED);
+        }else if(Colores.getValue()=="Verde"){
+            gc.setStroke(Color.GREEN);
+        }
 
     }
 
 
-
     public void limpiar(){
 
+        Colores.setValue("Colores");
         gc.clearRect(0,0,Dibujo.getWidth(),Dibujo.getHeight());
 
     }
--- EjerciciosFXML/Ejercicio33/src/main/java/com/example/ejercicio33/HelloController.java
+++ EjerciciosFXML/Ejercicio33/src/main/java/com/example/ejercicio33/HelloController.java
@@ -2,13 +2,47 @@ package com.example.ejercicio33;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.shape.Rectangle;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    @FXML public Rectangle rectangulo;
+    @FXML public Slider SliderRojo;
+    @FXML public Slider SliderVerde;
+    @FXML public Slider SliderAzul;
+    public Label textoRGB;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+
+        //HACEMOS QUE EL PROGRAMA ESTE ESCUCHANDO A LOS SLIDER PARA QUE CUANDO CAMBIEN DE VALOR SE ACTUALICE EL COLOR AL NUEVO VALOR DADO
+        SliderRojo.valueProperty().addListener((observable, oldValue, newValue) -> actualizarColorRectangulo());
+        SliderVerde.valueProperty().addListener((observable, oldValue, newValue) -> actualizarColorRectangulo());
+        SliderAzul.valueProperty().addListener((observable, oldValue, newValue) -> actualizarColorRectangulo());
+
+
+
+        //LLAMAMOS AL METODO DE CAMBIAR COLOR DEL RECTANGULO
+        actualizarColorRectangulo();
+
+    }
+
+    public void actualizarColorRectangulo() {
+
+        //GUARDAMOS EN VARIABLES LOS VALORES DE CADA SLIDER QUE INDICARAN UN COLOR U OTRO
+        int red = (int) SliderRojo.getValue();
+        int green = (int) SliderVerde.getValue();
+        int blue = (int) SliderAzul.getValue();
+
+        //CREAMOS UNA CADENA QUE INDICARA EL RGB CON LOS VALORES DE LOS SLIDERS USANDO EL %D PARA QUE VAYA COGIENDO
+        //EL VALOR DE CADA VARIABLE INDICADA EN ORDEN.
+        String color = String.format("rgb(%d,%d,%d)", red , green , blue);
+
+        //INDICAMOS EL COLOR QUE TENDRA NUESTRO RECTANGULO QUE SERA IGUAL AL COLOR GENERADO ANTERIORMENTE
+        rectangulo.setStyle("-fx-fill: " + color + ";");
+        //CADA VEZ QUE SE LLAME AL METODO SE LE ASIGNARA EL TEXTO AL TEXTORGB DE DEBAJO DEL RECTANGULO PARA INDICAR
+        //LOS VALORES ACTUALES DEL COLOR MOSTRADO.
+        textoRGB.setText(color);
     }
 }
